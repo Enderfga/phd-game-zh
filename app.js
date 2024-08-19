8fecc6cffbbc4678c46eacacf3577053aa559f06
Enderfga <enderfga@EnderfgadeMacBook-Pro.local>
2024-08-19 07:24:14 -0700

    port

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
-let port = 8000;
+let port = process.env.PORT || 8000; // 使用 Vercel 提供的端口
 let app = express();
-    app.use(express.static('./static'));
-    app.listen(port, () => {
-    console.log(`Server started on port ${port}.\nPlease navigate to http://localhost:${port} in your browser.`)
+app.use(express.static('./static'));
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.\nPlease navigate to http://localhost:${port} in your browser.`);
 });
+
